b9bf2fe21f4187240755a3f9d9f8423b50876a2c
gautamsen117 <gautamsen117@gmail.com>
2022-10-24 00:33:49 +0600

    explore-browser-api

---
+++ cookies.js
@@ -0,0 +1,9 @@
+const getCookiesName = name => {
+    const cookieString = document.cookie;
+    const cookiesParts = cookieString.split('; ');
+    const cookie = cookiesParts.find(c => c.includes(name));
+    if(cookie){
+        const cookieValue = cookie.split('=')[1]
+        return cookieValue;
+    }
+}
---
+++ prompt.js
@@ -0,0 +1,28 @@
+console.log('prompt connected');
+// alert('ki khabor dosto')
+const showAlert = () =>{
+    const num = Math.random() * 10;
+    console.log(num);
+    if(num < 5){
+        alert('ki khabor bondhu');
+    }
+}
+
+const askSomething = () => {
+    const decision = confirm('Are you coming Picnic?');
+    console.log(decision);
+    if(decision === true){
+        alert('dosto 500 tk bkash kor')
+    }
+    else{
+        console.log('DGM !! Door e giya mor');
+    }
+}
+
+const getUserInfo = () => {
+    const name = prompt('Tell us your name');
+    console.log(name);
+    if(!!name){
+        console.log('Welcome here', name);
+    }
+}
